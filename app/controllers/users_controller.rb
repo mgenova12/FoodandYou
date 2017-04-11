1e31e83db0f85cab418ec7a66a8dd63870ded2ba
Marco <mgenova03@gmail.com>
2017-04-11 09:39:43 -0400

    add sign up page

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  def new
+
+  end
+
+  def create
+    user = User.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation]
+    )
+
+    user.save
+
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password
+  
   has_many :added_foods
   has_many :foods, through: :added_foods
   has_many :meals, through: :added_foods
