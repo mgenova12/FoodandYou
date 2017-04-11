61522aac7fe9bf0b97f3792c19be705583c0e860
Marco <mgenova03@gmail.com>
2017-04-11 10:03:40 -0400

    create login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   get "/signup" => "users#new"
   post "/users" => "users#create"
 
-  # get "/login" => "sessions#new"
-  # post "/login" => "sessions#create"
-  # get "logout" => "sessions#destroy"
+  get "/login" => "sessions#new"
+  post "/login" => "sessions#create"
+  get "logout" => "sessions#destroy"
 
 end
