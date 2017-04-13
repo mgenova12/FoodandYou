53abc9a8ddaa98bded0d1ecba767e727f26c0563
Marco <mgenova03@gmail.com>
2017-04-13 09:44:22 -0400

    Add search for Foods

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,7 +2,11 @@ class DashboardsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-
+    if params[:search_food]
+      @food = Food.where("name = '#{params[:search_food]}'")
+    else 
+      @food = []
+    end
   end
 
   
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,14 @@ class UsersController < ApplicationController
       password_confirmation: params[:password_confirmation]
     )
 
-    user.save
+    if user.save
+      session[:user_id] = user.id
+      flash[:success] = 'Successfully created account!'
+      redirect_to '/dashboard'
+    else
+      flash[:warning] = 'Invalid email or password!'
+      redirect_to '/signup'
+    end    
 
   end
 end
