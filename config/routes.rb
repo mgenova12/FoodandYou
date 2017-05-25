3da518f34d61e8f27c07981870916f7fc1c30ccb
Marco <mgenova03@gmail.com>
2017-05-24 21:13:23 -0400

    change root to login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 resources :photos, only: [:new, :create, :index, :destroy]
-root to: 'photos#index'
+root to: 'sessions#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
   get '/dashboard' => 'dashboards#index'
 
   namespace :api do 
